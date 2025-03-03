e8c3ec8fc30b45f53ee23d6d7f0c8b4b73e432e6
Issam <issam.hannache@icloud.com>
2025-03-03 17:29:47 -0500

    feat: INFRA-987 initial commit

--- main.tf
+++ main.tf
@@ -1,13 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.0.0"
-    }
-  }
-  required_version = ">= 1.2.0"
-}
-
 module "security_groups" {
   source = "../terraform-aws-vault-security-groups"
 
